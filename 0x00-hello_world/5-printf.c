dd75989b41544bff48d4386b66db21b46a80ab15
yghari <ghariyouness90@gmail.com>
2023-03-16 02:30:05 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/* main - prints  the string
+ * the return will bee 0 if there is no prblm
+ * thas all
+ * */
+
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,");
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 
 }
