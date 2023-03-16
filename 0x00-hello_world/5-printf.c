b0e64519f1d80eb997747ec287dcbb64378a2980
yghari <ghariyouness90@gmail.com>
2023-03-16 02:32:58 -0700

    be a different person

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/* main - prints  the string
- * the return will bee 0 if there is no prblm
- * thas all
- * */
-
+/**
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	printf ("with proper grammar, but the outcome is a piece of art,\n");
