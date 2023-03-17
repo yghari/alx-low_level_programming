73d31c1b39a3010d8d4e6b89fad44d0f82122806
yghari <ghariyouness90@gmail.com>
2023-03-17 11:55:13 -0700

    all alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print all lower case alphg
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int num = 97;
+
+	while (num <= 122)
+		putchar(num++);
+	num = 65;
+	while (num <= 65)
+		putchar(num++);
+	putchar('\n');
+	return (0);
+}
