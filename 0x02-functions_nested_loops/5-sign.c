c74dfbcf6f6c07bf660dc4eac5f8c8df47638c6d
yghari <ghariyouness90@gmail.com>
2023-03-21 14:23:30 -0700

    positive or negative number or equals zero

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,26 +2,26 @@
 
 /**
  * print_sign - Print sign of a number.
- * @n: Th number to be checked
+ * @n: The number to be checked
  *
  * Return: 1 if n is greater than 0 , print +
  *  0 if n is zero .  print 0
  * -1 if n is less than zero . print -
  */
 
-int	print_sign(int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	else if (n < 0)
+	else
 	{
 		_putchar('-');
 		return (-1);
