4df67dce82906860761fc4546cd227114df5c4c7
yghari <ghariyouness90@gmail.com>
2023-03-21 15:05:42 -0700

    absolue value of a valuable

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - absolue velue of int
+ * @i : the variable from we gonna get the abs value
+ *
+ * Return: Always absolut balue of i.
+ */
+
+int	_abs(int i)
+{
+	if (i < 0)
+		return (-i);
+	else
+		return (i);
+	return (0);
+}
