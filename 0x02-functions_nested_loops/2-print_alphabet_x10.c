6867b9464dcf7a14197aac2d61dad9d325a0a8c1
yghari <ghariyouness90@gmail.com>
2023-03-21 13:12:33 -0700

    print alpha x 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase x 10,
+ * followed by a new line.
+ */
+
+void	print_alphabet_x10(void)
+{
+	char	i;
+	char	j;
+
+	i = 'a';
+	j = 0;
+	while (j < 10)
+	{
+		while (i <= 'z')
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar('\n');
+		i = 'a';
+		j++;
+	}
+}
