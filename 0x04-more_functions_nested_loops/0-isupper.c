8c72c45aa995f1c0c7cbc6ba7464ecf0610af5ff
yghari <ghariyouness90@gmail.com>
2023-03-23 11:14:49 -0700

    check in a character is digit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _isupper - check the code.
+ * _isupper - check a character if uper.
  *@c : the number to be checked
  *
  * Return:  0 if nont uppercase.
  * return : 1 if uppercase;
  */
 
-int	_isupper(int c)
+int	_isdigit(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - check nuber if digit or not.
+ *@c : the number to be checked
+ *
+ * Return:  1 is digit.
+ * return : 0 if n digit;
+ */
+
+int	_isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
