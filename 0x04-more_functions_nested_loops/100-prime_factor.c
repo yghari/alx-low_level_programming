07c86d8db5d05404936754d692129dcd98f9b9ce
yghari <ghariyouness90@gmail.com>
2023-03-24 02:12:12 +0000

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry line
+ * @void: no argument
+ * Return: alway 0
+ */
+int main(void)
+{
+	long int i;
+	long int n;
+
+	i = 612852475143;
+	n = 2;
+	while (n <= i)
+	{
+		if (i % n == 0)
+		{
+			i = i / n;
+			n--;
+		}
+		n++;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
