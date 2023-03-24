175edd25310c0763d76bd6e00651099768763c8b
yghari <ghariyouness90@gmail.com>
2023-03-23 19:34:46 -0700

    design

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,19 +11,19 @@ void print_number(int n)
 {
 	if (n == -2147483648)
 	{
-		_putchar('-');
-		_putchar('2');
+		putchar('-');
+		putchar('2');
 		print_number(147483648);
 	}
 	else if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		n = -n;
 		print_number(n);
 	}
 	else if (n < 10)
 	{
-		_putchar(n + 48);
+		putchar(n + 48);
 	}
 	else if (n >= 10)
 	{
