6780c5d32af9e4138e478d425c205f263b0ae65a
yghari <ghariyouness90@gmail.com>
2023-03-24 00:39:31 +0000

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,18 +12,12 @@ int	main(void)
 	i = 1;
 	while (i <= 100)
 	{
-		if ((i % 3) == 0 && (i % 5) == 0)
-		{
+		if (((i % 3) == 0) && ((i % 5) == 0))
 			printf("FizzBuzz");
-		}
 		else if ((i % 3) == 0)
-		{
 			printf("Fizz");
-		}
 		else if ((i % 5) == 0)
-		{
 			printf("Buzz");
-		}
 		else
 			printf("%d", i);
 
