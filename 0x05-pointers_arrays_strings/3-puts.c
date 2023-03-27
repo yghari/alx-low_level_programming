18a0d0ecd69f2e05c27abf09242ba19d1831ce9b
yghari <ghariyouness90@gmail.com>
2023-03-27 06:56:33 +0000

    printing a string followed by a newline

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - print a string to the stdout
+ * @str: the string
+ *
+ */
+
+void	_puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+
+}
