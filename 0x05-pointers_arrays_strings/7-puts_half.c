460eb31eeaa21fae639181b83f63a46dc1869151
yghari <ghariyouness90@gmail.com>
2023-03-27 09:56:01 +0000

    cutting and printing a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - print a half a string
+ * @str: the string that we going to cut
+ *
+ */
+
+void	puts_half(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	if ((i % 2) == 0)
+	{
+		i = i / 2;
+		while (str[i])
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+	else
+	{
+		i = (i - 1) / 2;
+		while (str[i])
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+	putchar('\n');
+}
