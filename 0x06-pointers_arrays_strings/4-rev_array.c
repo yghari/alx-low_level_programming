40f0f46840a8b6009d3b9ca0e13fc33451bb9fc1
yghari <ghariyouness90@gmail.com>
2023-03-29 19:53:08 +0000

    recoding rev_array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,14 +10,17 @@ void reverse_array(int *a, int n)
 {
 	int i;
 	int tmp;
+	int j;
 
 	i = 0;
+	j = n - 1;
 	tmp = 0;
 	while (i < n / 2)
 	{
 		tmp = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = tmp;
+		a[i] = a[j];
+		a[j] = tmp;
 		i++;
+		j--;
 	}
 }
