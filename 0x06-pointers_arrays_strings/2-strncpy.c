5c1032a1c5c26eb78fdc653e381e76ad855504b8
yghari <ghariyouness90@gmail.com>
2023-03-29 09:37:56 +0000

    strncpy function added

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - cating  of a string src to a string dest
+ * @src: the src string
+ * @dest: the dest string
+ * @n: the number of bytes to copy
+ * Return: a pointer to the the dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int is;
+	int id;
+
+	id = 0;
+	is = 0;
+	while (src[is])
+	{
+		if (is < n)
+			dest[is] = src[is];
+		is++;
+	}
+	dest[is] = '\0';
+	return (dest);
+}
