8918cd30acf6284bb59a6184f0021dd019096cb9
yghari <ghariyouness90@gmail.com>
2023-03-30 00:43:57 +0000

    cap_string update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,27 @@
-#include "stdio.h"
+#include "main.h"
 
 /**
- * *cap_string- capitalizes all words of a string.
- * @str: pointer on the sring
- * Return: return a pointer the str
+ * cap_string - Capitalizes all words of a string.
+ * @str: Pointer to a string.
+ *
+ * Return: Pointer to the modified string.
  */
-
 char *cap_string(char *str)
 {
 	int i;
-	int a;
 
-	i = 0;
-	a = 0;
-	while (str[i])
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == 32 || str[i] == 9 || str[i] == 10 || str[i] == 44
-				|| str[i] == 59 || str[i] == 46 || str[i] == 33 || str[i] == 63 
-				|| str[i] == 34 || str[i] == 40 || str[i] == 42 || str[i] == 123
-				|| str[i] == 125 )
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
+				str[i - 1] == '\n' || str[i - 1] == ',' ||
+				str[i - 1] == ';' || str[i - 1] == '.' ||
+				str[i - 1] == '!' || str[i - 1] == '?' ||
+				str[i - 1] == '\"' || str[i - 1] == '(' ||
+				str[i - 1] == ')' || str[i - 1] == '{' ||
+				str[i - 1] == '}')
 		{
-			i++;
-			a = 1;
+			str[i] = toupper(str[i]);
 		}
-		if ((str[i] >= 'A' && str[i] <= 'Z'))
-		{
-			a = 0;
-		}
-		if ((str[i - 1] >= '0' && str[i - 1] <= '9'))
-		{
-			a = 0;
-		}
-		if (a == 1 && (str[i] >= 'a' && str[i] <= 'z'))
-		{
-			str[i] = str[i] - 32;
-			a = 0;
-		}
-		i++;
 	}
+	return str;
 }
