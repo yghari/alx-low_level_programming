ca520019ce3f3e1089fbd9239fa75c558b7539b1
yghari <ghariyouness90@gmail.com>
2023-04-03 05:24:39 +0000

    coping memory from src to dest

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - copying  of a string src to a string dest
+ * @s: pointer to the src
+ * @b: the charactere
+ * @n: the size or
+ * Return: a pointer to the the dest
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int count;
+
+	count = 0;
+	while (n > 0)
+	{
+		*(s + count) = b;
+		count++;
+		n--;
+	}
+	return (s);
+}
