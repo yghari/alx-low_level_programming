9ff899127275d0a6e78740e6ccaf59cf45de404e
yghari <ghariyouness90@gmail.com>
2023-04-04 21:52:00 +0000

    set _string

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,27 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_diagsums - prints the sum of the two diagonals.
- * @a: the 2d array
- * @size: the size of the array
+ * print_diagsums - diagram
+ * @a: String
+ * @size: size of array
+ * Return: NULL
  */
 
-void print_diagsums(int *a, int size)
+void	print_diagsums(int *a, int size)
 {
-	int i;
-	unsigned int sum_1;
-	unsigned int sum_2;
+	int	i;
+	int	j;
+	int	sum1;
+	int	sum2;
 
 	i = 0;
-	sum_1 = 0;
-	sum_2 = 0;
-
+	j = 0;
+	sum1 = 0;
+	sum2 = 0;
 	while (i < size)
 	{
-		sum_1 += a[(size * i) + i];
-		sum_2 += a[(size * (i + 1)) - (i + 1)];
+		sum1 += a[(size + 1) * i];
 		i++;
 	}
-	printf("%d, %d\n", sum_1, sum_2);
+	while (j < size)
+	{
+		sum2 += a[(size - 1) * (j + 1)];
+		j++;
+	}
+	printf("%d, %d\n", sum1, sum2);
 }
