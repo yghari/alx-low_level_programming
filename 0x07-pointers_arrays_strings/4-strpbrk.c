1947cfb6692e60aed84f56ca19700c2ad93e657a
yghari <ghariyouness90@gmail.com>
2023-04-04 00:15:14 +0000

    ading strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * _strpbrk - locates a character in a string.
  * @s: String
  * @accept: character
  * Return: NULL
  */
+
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
