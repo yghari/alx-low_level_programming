fa04b86e6dbaf74ad6fbf60f2df5434b05a01ba4
yghari <ghariyouness90@gmail.com>
2023-04-04 08:40:13 +0000

    print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * print_diagsums - diagram
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stddef.h>
-
+#include <string.h>
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
