e09eddba3ba9f1d75e1159dd6a7157021046e2af
yghari <ghariyouness90@gmail.com>
2023-04-04 00:12:49 +0000

    ading strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,20 +7,20 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-     int i;
-     int j;
+	int i;
+	int j;
 
-     i = 0;
-     while (s[i])
-     {
-          j = 0;
-          while (accept[j])
-          {
-               if (s[i] == accept[j])
-                    return (s + i);
-               j++;
-          }
-          i++;
-     }
-     return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		j = 0;
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
 }
