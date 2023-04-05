5aac34cb398ab8965245aeb32a6e3bd435ab1a83
yghari <ghariyouness90@gmail.com>
2023-04-05 20:13:07 +0000

    power of a num added

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - put recursion
+ * @x: var
+ * @y: var
+ * Return: a pointer to the memory area @s
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+
+}
