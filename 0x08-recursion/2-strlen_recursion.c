daa6f00802e6827e7683040d7a11d15fbb6e309c
yghari <ghariyouness90@gmail.com>
2023-04-05 09:19:56 +0000

    ading strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - put recursion
+ * @s: poiter to the memory area
+ * Return: a pointer to the memory area @s
+ */
+
+int _strlen_recursion(char *s)
+{
+     if (*s == '\0')
+          return(0);
+     else
+          return(1 + _strlen_recursion(s + 1));
+}
