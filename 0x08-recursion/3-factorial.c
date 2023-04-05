006e7bf17898767e60103f063738d1cefc6204f1
yghari <ghariyouness90@gmail.com>
2023-04-05 10:24:36 +0000

    factorial added

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - put recursion
+ * @n: the number poiter to the memory area
+ * Return: a pointer to the memory area @s
+ */
+
+int factorial(int n)
+{
+	if (n > 0)
+		return (n * factorial(n - 1));
+	if (n == 0)
+		return(1);
+	else if (n < 0)
+		return(-1);
+}
