fd5560ec91f5aa7a72684fc464e6dc6b7c1f3a99
yghari <ghariyouness90@gmail.com>
2023-04-11 05:43:07 +0000

    strconca concat two arrays

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,30 +10,28 @@
 
 char *str_concat(char *s1, char *s2)
 {
-  unsigned int i;
-  unsigned int j;
-  char *str;
+	unsigned int i;
+	unsigned int j;
+	char *str;
 
-  if (s1 == NULL || s2 == NULL)
-    return (NULL);
-  i = strlen(s1);
-  j = strlen(s2);
-  str = (char *)malloc(sizeof(char) * (i + j + 1));
-  if (!str)
-    return (NULL);
-  i = 0;
-  while (s1[i])
-  {
-    str[i] = s1[i];
-    i++;
-  }
-  j = 0;
-  while (s2[j])
-  {
-    str[i] = s2[j];
-    i++;
-    j++;
-  }
-  str[i + 1] = '\0';
-  return (str);
+	i = strlen(s1);
+	j = strlen(s2);
+	str = (char *)malloc(sizeof(char) * (i + j + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i + 1] = '\0';
+	return (str);
 }
