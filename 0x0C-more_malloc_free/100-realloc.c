29dd32df557b5f94ad04e95f74f3ee78ce2324ce
yghari <ghariyouness90@gmail.com>
2023-04-15 00:58:28 +0000

    realloc and mul

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,67 +1,64 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
- * _memcpy - fills memory with a constant byte
- * @src: poiter to the memory area
- * @dest: number of bytes
- * @n: the constant byte
- * Return: a pointer to the memory area @s
+ * __memcpy- copies memory area.
+ * @dest: estination
+ * @src: source
+ * @n: number of bytes
+ * Return: dest
  */
 
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *__memcpy(char *dest, char *src, unsigned int n)
 {
-	int			i;
+	char *psrc = src;
+	char *pdest = dest;
 
-	i = 0;
-	if (!dest && !src)
-		return (NULL);
-	while (n > 0)
+	while (n-- > 0)
 	{
-		dest[i] = src[i];
-		i++;
-		n--;
+		*pdest++ = *psrc++;
 	}
+
 	return (dest);
 }
 
 /**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory block to be reallocated
- * @old_size: size of the previously allocated memory block
- * @new_size: size of the new memory block
- *
- * Return: pointer to the reallocated memory block, or NULL on failure
- */
+ * _realloc - reallocates a memory block
+ * @ptr: pointer to the old memory
+ * @old_size: old memory size
+ * @new_size: new memory size
+ * Return: pointer to new allocated memory.
+*/
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
+	void *p;
 
-	/* Step 1: Check if ptr is NULL */
-	if (ptr == NULL)
+	if (new_size == old_size)
+		return (ptr);
+	if (!ptr)
 	{
-		new_ptr = malloc(new_size);
-		return (new_ptr);
+		p = malloc(new_size);
+		if (!p)
+			return (NULL);
+		return (p);
 	}
-
-	/* Step 2: Check if new_size is zero */
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	/* Step 3: Allocate memory for new_size */
-	new_ptr = malloc(new_size);
-	if (new_ptr == NULL)
-	{
+	p = malloc(new_size);
+
+	if (!p)
 		return (NULL);
-	}
-
-	/* Step 4: Copy contents of ptr to new block */
-	_memcpy(new_ptr, ptr, old_size < new_size ? old_size : new_size);
-
-	/* Step 5: Free old block */
+	if (new_size < old_size)
+		__memcpy(p, ptr, new_size);
+	else
+		__memcpy(p, ptr, old_size);
 	free(ptr);
-
-	/* Step 6: Return pointer to new block */
-	return (new_ptr);
+	return (p);
 }
