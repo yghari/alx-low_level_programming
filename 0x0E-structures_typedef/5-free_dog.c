c4e4cb002fd29980250f40f3cc6771789adb046b
yghari <ghariyouness90@gmail.com>
2023-04-17 13:56:22 +0000

    free the variable in the struct

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,8 +1,18 @@
 #include "dog.h"
-
+/**
+ * free_dog - Frees memory allocated for a dog_t object.
+ * @d: Pointer to the dog_t object to free.
+ *
+ * Description: This function frees the memory allocated for the `name` and
+ * `owner` character arrays of a dog_t object, and then frees the memory
+ * allocated for the dog_t object itself. If `d` is NULL, the function does
+ * nothing.
+ */
 void free_dog(dog_t *d)
 {
-  free(d->owner);
-  free(d->name);
+	if (d == NULL)
+		return;
+	free(d->owner);
+	free(d->name);
 	free(d);
 }
