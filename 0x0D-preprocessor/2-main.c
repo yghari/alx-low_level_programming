3cd5f3cc5c7b4dfc2a17476d71b2d7e2e05303ee
yghari <ghariyouness90@gmail.com>
2023-04-17 14:16:58 +0000

    preprocessing

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prins the name of the file
+ *
+ * Return: only
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+salmu alaykum
