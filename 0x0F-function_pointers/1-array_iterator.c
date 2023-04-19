bedb0e03c767f9b746c561b2248efacf8b2126c2
yghari <ghariyouness90@gmail.com>
2023-04-19 23:46:26 +0000

    adding array iterrator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates over an array and performs an action
+ * @array: the array to iterate over
+ * @size: the size of the array
+ * @action: a pointer to the function to perform on each element
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	i = 0;
+	if (array != NULL && action != NULL)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+}
