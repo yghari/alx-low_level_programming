c433064b123c7eadcdc75e2a37e48d0f4f6426f5
yghari <ghariyouness90@gmail.com>
2023-04-20 16:10:18 +0000

    printf demo haha

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,37 +7,36 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i;
-	char *str;
+	unsigned int i = 0;
+	char *str, *sep = "";
 
 	va_start(args, format);
 
-	for (i = 0; format && format[i]; i++)
+	while (format && format[i])
 	{
-		if (i != 0)
-			printf(", ");
-
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(args, int));
+				printf("%s%c", sep, va_arg(args, int));
 				break;
 			case 'i':
-				printf("%d", va_arg(args, int));
+				printf("%s%d", sep, va_arg(args, int));
 				break;
 			case 'f':
-				printf("%f", va_arg(args, double));
+				printf("%s%f", sep, va_arg(args, double));
 				break;
 			case 's':
 				str = va_arg(args, char *);
-				if (str == NULL)
-					printf("(nil)");
-				else
-					printf("%s", str);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", sep, str);
 				break;
 			default:
+				i++;
 				continue;
 		}
+		sep = ", ";
+		i++;
 	}
 
 	printf("\n");
