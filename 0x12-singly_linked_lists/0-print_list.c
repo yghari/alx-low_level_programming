3be7e109f032819705e6700ec352a6693e71afd1
yghari <ghariyouness90@gmail.com>
2023-04-27 15:20:29 +0100

    singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,13 +15,9 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str != NULL)
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
+			printf("[%u] %s\n", h->len, h->str);
 		else
-		{
 			printf("[0] (nil)\n");
-		}
 		h = h->next;
 		count++;
 	}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LIST_H
+#define _LIST_H
 
 #include <stdio.h>
 #include <unistd.h>
