6fc5dca7b83c140f953dc7c2cab3e50894fe8805
yghari <ghariyouness90@gmail.com>
2023-04-27 15:15:21 +0100

    singly linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all the elements of a linked list.
+ * @h: A pointer to the head of the linked list.
+ *
+ * Return: The number of nodes in the linked list.
+ *         If h is NULL, returns 0.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,28 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * struct list_t - A linked list node with a string value and a length field.
+ * @str: A pointer to a string value for the node.
+ * @len: The length of the string value, in bytes.
+ * @next: A pointer to the next node in the linked list.
+ *
+ * Description: This struct defines a node in a singly-linked list.
+ *              Each node contains a string value, a length field,
+ *              and a pointer to the next node in the list.
+ */
+
+typedef struct list_t
+{
+	char *str;
+	size_t len;
+	struct list_t *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
