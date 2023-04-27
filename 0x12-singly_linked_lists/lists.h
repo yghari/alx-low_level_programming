7ff43c0dd1abe3aea3c491f56aa46cc9c04f4b5d
yghari <ghariyouness90@gmail.com>
2023-04-27 20:52:57 +0100

    frees a node

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -29,5 +29,6 @@ list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
 int _strlen(const char *s);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
