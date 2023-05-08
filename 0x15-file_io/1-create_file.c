220015fe2388b4a56646dfc90b37f1f8b95b8f11
yghari <ghariyouness90@gmail.com>
2023-05-08 23:49:12 +0100

    Speak gently, she can hear

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,6 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-
 	if (text_content)
 	{
 		for (len = 0; text_content[len];)
@@ -23,8 +22,6 @@ int create_file(const char *filename, char *text_content)
 	w = write(o, text_content, len);
 	if (o == -1 || w == -1)
 		return (-1);
-
 	close(o);
-
 	return (1);
 }
